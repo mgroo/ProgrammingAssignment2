6e2f24916a42479c4c8b96599ac95f3e41be42ca
mgroo <matthijs@nextbyte.nl>
2014-04-25 15:46:56 +0200

    Initial version. Modified the code of the available makeVector and cachemean functions to be able to invert a matrix. and store te inverted matrix in a cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a getter and setter of the cache and the matrix itself
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+        x <<- y
+        m <<- NULL
+      }
+      get <- function() x
+      setinv <- function(mean) m <<- mean
+      getinv <- function() m
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will check if the result is cached and return it, if so. 
+## If not it it will perform the solve function to inverse the matrix and put it in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      m <- x$getinv()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+      data <- x$get()
+      ##m <- mean(data, ...)
+      m <- solve(data, ...)
+      x$setinv(m)
+      m 
 }
